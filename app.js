6595977dd8fbb6c72c9b450e89018c0867bf92a7
Cai heng <ianyaheng822@gmail.com>
2020-05-27 12:41:13 +1000

    add attack functions

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
   data:{
     playHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns:[]
   },
   methods:{
     startGame:function(){
@@ -21,19 +22,70 @@ new Vue({
       if (this.checkWin()) {
           return;
       }
-
       this.monsterAttacks();
 
     },
     specialAttack:function(){
-      
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+          isPlayer: true,
+          text: 'Player hits Monster hard for ' + damage
+      });
+      if (this.checkWin()) {
+          return;
+      }
+      this.monsterAttacks();
     },
+
     heal:function(){
-      
+      if(this.playHealth<=90){
+        this.playHealth +=10;
+      }else{
+        this.playHealth = 100;
+      }
+      this.turn.unshift({
+        isPlayer: true,
+        text:"Player heals for 10"
+      });
+
     },
     giveUp:function(){
-      
-    }
+      this.gameIsRunning=false;
+    },
+
+    calculateDamage:function(max,min){
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+
+    monsterAttacks:function(){
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
+      this.checkWin();
+      this.turns.unshift({
+          isPlayer: false,
+          text: 'Monster hits Player for ' + damage
+      });
+    },
+
+    checkWin: function() {
+      if (this.monsterHealth <= 0) {
+          if (confirm('You won! New Game?')) {
+              this.startGame();
+          } else {
+              this.gameIsRunning = false;
+          }
+          return true;
+      } else if (this.playerHealth <= 0) {
+          if (confirm('You lost! New Game?')) {
+              this.startGame();
+          } else {
+              this.gameIsRunning = false;
+          }
+          return true;
+      }
+      return false;
+  }
   }
 
 });
